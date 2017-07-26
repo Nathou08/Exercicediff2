8b65ea5dbda708e9afda43935352c72089e85ec3
UTILISATEUR <UTILISATEUR@192.168.1.167>
2017-07-26 11:17:26 +0200

    structure

--- public_html/anim.js
+++ public_html/anim.js
@@ -1,4 +1,15 @@
 function converter(){
-    var x = document.getElementById("saisie").value;
+    var euro = parseInt(document.getElementById("saisie").value);
+    var cuivre = 0.1;
+    var argent = 10;
+    var or = 100;
+    var platine = 5000;
+    var mithril = 100000;
+    
+    if(euro < argent){
+        euro+cuivre
+    }
+    
+    alert(result);
 }
 
